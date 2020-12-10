281a7be01feeb0e6d52302ff39dca46821ee12c5
gyq <gyq@IandeMacBook-Pro.local>
2020-12-11 01:42:41 +0800

    重做了了导航页

--- src/components/PagePresent.vue
+++ src/components/PagePresent.vue
@@ -1,38 +1,39 @@
 <template>
-  <div>
     <el-menu
       :default-active="currentPath"
       router
       mode="horizontal"
-      background-color="#4f4f4f"
-      text-color="white"
-      active-text-color="#00A2E9"
+      background-color="white"
+      text-color="#222"
+      active-text-color="red"
       style="min-width: 1300px">
       <el-menu-item v-for="(item,i) in navList" :key="i" :index="item.name">
         {{ item.navItem }}
       </el-menu-item>
+      <span style="position: absolute;padding-top: 20px;right: 45%;font-size: 20px;font-weight: bold">PKU Sports</span>
 
       <el-button
-              type="primary"
-              size="small"
-              style="width: 9%;background: #FF9966;border: none;position:absolute;margin-top: 14px;right:10%"
-              v-on:click="logout">
-              退出登录
+          type="primary"
+          size="small"
+          style="width: 9%;background: #696969;border: none;position:absolute;margin-top: 14px;right:3%"
+          v-on:click="logout">
+        退出登录
       </el-button>
+
     </el-menu>
-  </div>
 </template>
 
 <script>
   export default {
-    name: 'NavMenu',
+    name: 'PagePresent',
     data () {
       return {
         navList: [
-          {name: '/homepage', navItem: '首页'},
-          {name: '/t2', navItem: '测试2'}
-        ],
-        keywords: ''
+          {name: '/index', navItem: '首页'},
+          {name: '/result', navItem: '赛果'},
+          {name: '/upload', navItem: '上传'},
+          {name: '/admin', navItem: '个人中心'}
+        ]
       }
     },
     computed: {
@@ -62,5 +63,4 @@
   span {
     pointer-events: none;
   }
-
 </style>
