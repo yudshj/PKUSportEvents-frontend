37a3c909e1b39ca282c71f121c9431f94f7e668c
IanWalls <ianwalls@pku.edu.cn>
2020-12-11 16:56:03 +0800

    重构了代码

---
+++ src/components/index/IndexPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div el-container direction="vertical" class="demo-box">
+    <div el-header>
+      <page-head></page-head>
+    </div>
+    <div el-main></div>
+  </div>
+</template>
+
+<script>
+import PageHead from '@/components/Tools/PageHead'
+
+export default {
+  name: "IndexPage",
+  components: {PageHead},
+  comments: {PageHead}
+}
+</script>
+
+<style scoped>
+
+</style>
